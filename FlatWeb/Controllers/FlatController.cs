d2b8f4d6f24bed5011e81f59767cea46d9400cb7
Marek Grys <marekgrys01@gmail.com>
2024-01-10 01:03:45 +0100

    Added 2 basic endpoints to test responsiveness

---
+++ FlatWeb/Controllers/FlatController.cs
@@ -0,0 +1,39 @@
+﻿using FlatWeb.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlatWeb.Controllers
+{
+    [Route("api/flats")]
+    public class FlatController : ControllerBase
+    {
+        private readonly FlatWebDbContext _dbContext;
+
+        public FlatController(FlatWebDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Flat>> GetAll()
+        {
+            var flats = _dbContext.Flats.ToList();
+
+            return Ok(flats);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Flat> Get([FromRoute]int id)
+        {
+            var flats = _dbContext.Flats.FirstOrDefault(x => x.Id == id);
+
+            if(flats == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flats);
+        }
+    }
+}
